d790561f21eed762f6c4ca10d43825ee0b4a02d0
quiambao970 <quiambao970@gmail.com>
2025-03-18 21:17:37 +0800

    work

--- Components/SiteFooter/functions.php
+++ Components/SiteFooter/functions.php
@@ -2,6 +2,7 @@
 
 namespace Flynt\Components\SiteFooter;
 
+use Flynt\Utils\Asset;
 use Timber\Timber;
 
 add_action('init', function (): void {
@@ -12,6 +13,36 @@ add_action('init', function (): void {
 
 add_filter('Flynt/addComponentData?name=SiteFooter', function (array $data): array {
     $data['menu'] = Timber::get_menu('navigation_footer') ?? Timber::get_pages_menu();
+
+    $data['facebook'] = [
+        'src' => Asset::requireUrl('assets/images/footer_social/facebook.svg'),
+        'alt' => 'facebook'
+    ];
+
+    $data['instagram'] = [
+        'src' => Asset::requireUrl('assets/images/footer_social/instagram.svg'),
+        'alt' => 'instagram'
+    ];
+
+    $data['linkedin'] = [
+        'src' => Asset::requireUrl('assets/images/footer_social/linkedIn.svg'),
+        'alt' => 'linkedin'
+    ];
+
+    $data['pinterest'] = [
+        'src' => Asset::requireUrl('assets/images/footer_social/pinterest.svg'),
+        'alt' => 'pinterest'
+    ];
+
+    $data['twitter'] = [
+        'src' => Asset::requireUrl('assets/images/footer_social/twitter.svg'),
+        'alt' => 'twitter'
+    ];
+
+    $data['youtube'] = [
+        'src' => Asset::requireUrl('assets/images/footer_social/youtube.svg'),
+        'alt' => 'youtube'
+    ];
 
     return $data;
 });
