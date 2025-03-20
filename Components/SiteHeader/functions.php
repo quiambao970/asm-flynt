dbee62819b1b489bdf661d771a2d03c195e59689
isabelaranguren <isabelarangurenl01@gmail.com>
2025-03-20 13:38:55 -0500

    Styles

--- Components/SiteHeader/functions.php
+++ Components/SiteHeader/functions.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Flynt\Components\SiteHeader;
-
 use Flynt\Utils\Asset;
 use Timber\Timber;
 use Flynt\Utils\Options;
@@ -30,52 +28,42 @@ add_filter('Flynt/addComponentData?name=SiteHeader', function (array $data): arr
     $data['pinterest_url'] = Options::getGlobal('Website Settings', 'pinterest_url');
     $data['instagram_url'] = Options::getGlobal('Website Settings', 'instagram_url');
     $data['youtube_url'] = Options::getGlobal('Website Settings', 'youtube_url');
-
     $data['logo'] = [
         'src' => Asset::requireUrl('assets/images/asm-logo.webp'),
         'alt' => get_bloginfo('name')
     ];
-
     $data['bbb'] = [
         'src' => Asset::requireUrl('assets/images/footer/bbb.svg'),
         'alt' => 'Better Business bureau'
     ];
-
     $data['down_arrow'] = [
         'src' => Asset::requireUrl('assets/images/header/down-arrow.svg'),
         'alt' => 'down-arrow'
     ];
-
     $data['facebook'] = [
         'src' => Asset::requireUrl('assets/images/header/facebook.svg'),
         'alt' => 'facebook'
     ];
-
     $data['instagram'] = [
         'src' => Asset::requireUrl('assets/images/header/instagram.svg'),
         'alt' => 'instagram'
     ];
-
     $data['phone'] = [
         'src' => Asset::requireUrl('assets/images/header/phone.svg'),
         'alt' => 'phone'
     ];
-
     $data['pinterest'] = [
         'src' => Asset::requireUrl('assets/images/header/pinterest.svg'),
         'alt' => 'pinterest'
     ];
-
     $data['right_arrow'] = [
         'src' => Asset::requireUrl('assets/images/header/right-arrow.svg'),
         'alt' => 'right-arrow'
     ];
-
     $data['youtube'] = [
         'src' => Asset::requireUrl('assets/images/header/youtube.svg'),
         'alt' => 'youtube'
     ];
-
     return $data;
 });
 
